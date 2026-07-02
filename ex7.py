3e732eafb97daf307c878f9fb5c8d949e47e1ccc
sidibe kadidjatou <sidibekdj30@gmail.com>
2026-07-02 19:55:36 +0000

    Exercices 7 done with study drills

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# this print a sentence 
+print("Mary had a little lamb.")
+# this print a sentence
+print("Its fleece was white as {}.".format('snow'))
+# this print another sentence
+print("And everywhere that Mary went.")
+# this print 10 periods
+print("." * 10) # what'd that do?
+
+# those store letters from line 10 to line 12
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens 
+# this print a word thanks to the combining of strings from line 10 to line 16
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# this print another different word but this one will be combining to the previous one after associating strings from line 17 to line 22
+print(end7 + end8 + end9 + end10 + end11 + end12)
