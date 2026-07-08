684bed0c16edfa2870ace2c407a31b5d71a28624
sidibe kadidjatou <sidibekdj30@gmail.com>
2026-07-08 22:01:57 +0000

    Exercice 9 done with study drills

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# This stores the days of week .
+days = "Mon Tur Wed Thu Fri Sat Sun"
+# This stores the months of year .
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# This print the days of week .
+print("Here are the days:", days)
+# This print the months of year .
+print("Here are the months:", months)
+
+# Here is a print that contains triple-quotes .
+print("""
+      There's something going on here.
+      with the three double-quotes .
+      We'll be able to type as much as we like.
+      Even 4 lines if we want, or 5, or 6 .
+      """)
