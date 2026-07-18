05aa602fe092abe8abb0800146fb07581c8b0726
sidibe kadidjatou <sidibekdj30@gmail.com>
2026-07-18 23:10:01 +0000

    Exercice 12 done with study drills

---
+++ ex12.py
@@ -0,0 +1,5 @@
+# This is a simple and shorter way to use input() compared to ex11.py  .
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weight?")
+print(f"So, you're {age} old,{height} tall and {weight} heavy.")
